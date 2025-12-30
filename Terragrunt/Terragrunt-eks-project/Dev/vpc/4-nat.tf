35c314bcab97931f21b4a14d9c41930ee6d80284
manasthorat <manasthorat09@gmail.com>
2025-12-30 15:42:17 +0530

    Add complete Python examples for design principles

---
+++ Terragrunt/Terragrunt-eks-project/Dev/vpc/4-nat.tf
@@ -0,0 +1,17 @@
+resource "aws_eip" "nat" {
+  vpc = true
+  tags = {
+    Name = "dev-nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  subnet_id = "aws_subnet.public_us_east_1a.id"
+  allocation_id = "aws_eip.nat.id"
+  tags = {
+    Name = "dev-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
